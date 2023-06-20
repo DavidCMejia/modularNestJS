ce75b17dad686062506e34fef589d5d26c877133
David <dcmejiaz@geosat.com.co>
2023-06-20 08:56:24 -0500

    apiKEy con injectable, parecido a useContext

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = '12345634';
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // como AppService tiene por defecto el useClass con el scope de singleton, no hay necesidad de {}
+    // pero para el apiKey, como no tiene scope, hay que ponerlo
+    {
+      provide: 'API_KEY',
+      useValue: API_KEY,
+    },
+  ],
 })
 export class AppModule {}
