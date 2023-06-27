c3cb037e62909baa6ff36fd6d8a54baf8678a322
David <dcmejiaz@geosat.com.co>
2023-06-27 08:26:37 -0500

    autoDocumentacion con Swagger

--- src/products/entities/order.entity.ts
+++ src/products/entities/order.entity.ts
@@ -1,7 +1,9 @@
 import { User } from 'src/users/entities/user.entity';
 import { Product } from './product.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class Order {
+  @ApiProperty()
   date: Date;
   user: User;
   products: Product[];
