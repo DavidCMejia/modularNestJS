26957817ca97c01c5f3b7e6965c9763356501b15
David <dcmejiaz@geosat.com.co>
2023-07-13 08:38:31 -0500

    iniciando typeORM

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,7 @@ import { ConfigType } from '@nestjs/config';
 import { Global, Module } from '@nestjs/common';
 import { Client } from 'pg';
 import config from '../config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const DB = 'postgres';
 const client = new Client({
@@ -20,6 +21,22 @@ client.connect();
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, dbName, password, port } = configService.postgres;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: dbName,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'DB_CONNECTION',
@@ -36,13 +53,12 @@ client.connect();
           password,
           port,
         });
-        console.log('🚀 ~ client:', client);
         client.connect();
         return client;
       },
       inject: [config.KEY],
     },
   ],
-  exports: ['DB_CONNECTION', 'PG'],
+  exports: ['DB_CONNECTION', 'PG', TypeOrmModule],
 })
 export class DatabaseModule {}
