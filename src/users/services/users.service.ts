faf9a737986c856a3eb1652b5be260415d612e00
David <dcmejiaz@geosat.com.co>
2023-07-26 07:07:12 -0500

    ajustando brand service

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -66,19 +66,4 @@ export class UsersService {
       products: await this.productsService.findAll(),
     };
   }
-
-  getTasks() {
-    // 👈 new method
-    return {
-      message: 'Aca van las tareas',
-    };
-    // return new Promise((resolve, reject) => {
-    //   this.clientPg.query('SELECT * FROM tasks', (err, res) => {
-    //     if (err) {
-    //       reject(err);
-    //     }
-    //     resolve(res.rows);
-    //   });
-    // });
-  }
 }
