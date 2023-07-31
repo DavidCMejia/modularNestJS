e80f49b7ceb156663d5a8ca3ff0c6510dd5dcd82
David <dcmejiaz@geosat.com.co>
2023-07-31 06:57:59 -0500

    ajustando categories servcs

--- src/products/entities/category.entity.ts
+++ src/products/entities/category.entity.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BasicEntity } from '../../database/basic.entity';
 
 @Entity()
-export class Category {
+export class Category extends BasicEntity {
   @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
